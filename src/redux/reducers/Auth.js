28a0ccddf128f8fb6057057bf35a374a9970fd31
zainulmbrk <zainulmbrk.id@gmail.com>
2022-09-14 20:54:42 +0800

    feat: fixing pages

--- src/redux/reducers/Auth.js
+++ src/redux/reducers/Auth.js
@@ -1,50 +1,78 @@
 //LOGIN
 const initialState = {
-    loading: false,
-    data: {
-        "token": null,
-        "user_id": null,
-        "role": null,
-    },
-    error: null,
-    isLogin: false
+  loading: false,
+  data: {
+    token: null,
+    user_id: null,
+    role: null,
+  },
+  error: null,
+  isLogin: false,
 }
 
-export const LoginFetch = (state=initialState, action={})=>{
-    switch(action.type){
-        case "LOGIN_REQUEST":
-            return{...state, loading:true}
-        case "LOGIN_ERROR":
-            return{...state, loading:false, data: state.data, error: action.payload, isLogin: false}
-        case "LOGIN_SUCCESS":
-            return{...state, loading:false, data: action.payload, error:null, isLogin: true}
-        case "AUTH_LOGOUT":
-            return{loading: false, data: {"user_id": null, "token": null }, error: null, isLogin: false}
-        default:
-            return state
-    }
+export const LoginFetch = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case 'LOGIN_REQUEST':
+      return { ...state, loading: true }
+    case 'LOGIN_ERROR':
+      return {
+        ...state,
+        loading: false,
+        data: state.data,
+        error: action.payload,
+        isLogin: false,
+      }
+    case 'LOGIN_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+        error: null,
+        isLogin: true,
+      }
+    case 'AUTH_LOGOUT':
+      return {
+        loading: false,
+        data: { user_id: null, token: null },
+        error: null,
+        isLogin: false,
+      }
+    default:
+      return state
+  }
 }
-
 
 //REGISTER
-const initialStates ={
-    loading: false,
-    data: {},
-    message:null,
-    error:null,
-    isRegister: false
+const initialStates = {
+  loading: false,
+  data: {},
+  message: null,
+  error: null,
+  isRegister: false,
 }
 
-export const RegisterFetch = (state=initialStates, action={})=>{
-    switch(action.type){
-        case "REGISTER_REQUEST":
-            return{...state, loading:true}
-        case "REGISTER_ERROR":
-            return{...state, loading:false, data: state.data, message: action.payload, error:action.payload, isRegister: false}
-        case "REGISTER_SUCCESS":
-            return{...state, loading:false, data: action.payload, error:null, isRegister:true}
-        default:
-            return state
-    }
+export const RegisterFetch = (state = initialStates, action = {}) => {
+  switch (action.type) {
+    case 'REGISTER_REQUEST':
+      return { ...state, loading: true }
+    case 'REGISTER_ERROR':
+      return {
+        ...state,
+        loading: false,
+        data: state.data,
+        message: action.payload,
+        error: action.payload,
+        isRegister: false,
+      }
+    case 'REGISTER_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+        error: null,
+        isRegister: true,
+      }
+    default:
+      return state
+  }
 }
-
